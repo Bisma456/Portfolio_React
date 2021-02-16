d391ffdaf729e4ec9ff05c60421c8cd10ba70f42
muhammadnaeemmadni <muhammadnaeemmadni786@gmail.com>
2021-02-16 10:28:54 +0500

    'done"

--- src/components/Social.js
+++ src/components/Social.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class Social extends Component {
     render() {
         return (
-            <div class="social">
-            <a href="https://github.com/Bisma456" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-            <a href="https://Instagram.com/bisma_456" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.facebook.com/zoe.hz.315" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <div className="social">
+            <a href="https://github.com/Bisma456" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+            <a href="https://www.instagram.com/bella.web911/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/zoe.hz.315" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-f"></i></a>
             </div>
             )
         }
--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -6,7 +6,7 @@ class Contact extends Component {
             <div className="condiv">
             <h1 className="subtopic">Contact Me</h1>
             <h3>Email  :   prop.web911@gmail.com</h3>
-            <h3>Instagram   :   @Bisma_456</h3>
+            <h3>Instagram   :   @bella.web911</h3>
             <Social />
             </div>
             )
